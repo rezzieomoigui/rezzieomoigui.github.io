5d4ba424c49ed00b02e7b99a83c46cbf4d109a41
rezzie omoigui <rezzieomoigui.github.io>
2025-05-21 11:15:01 -0400

    u

--- exercises/04js/js/intro.js
+++ exercises/04js/js/intro.js
@@ -1,6 +1,11 @@
 /*when button clicked write to a paragraph*/
+
+/*
 const writeParagraph = () => {
     console.log("Hello Everyone!");
 }; 
 
 document.getElementById("btn-write").onclick = writeParagraph;
+*/
+
+document.getElementById("btn-write").onclick = () => {};
