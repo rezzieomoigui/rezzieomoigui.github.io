350e3cbf5adcabeb0e999a2ab0306e02c05e0e72
rezzie omoigui <rezzieomoigui.github.io>
2025-05-21 11:29:41 -0400

    u

--- exercises/04js/js/intro.js
+++ exercises/04js/js/intro.js
@@ -12,6 +12,8 @@ document.getElementById("btn-write").onclick = () => {
     document.getElementById("writep").innerHTML = "You clicked the button";
 
     /* change the button text to say done */
-    document.getElementById("btn-write").onclick = (event) => {
+document.getElementById("btn-write").onclick = (event) => {
     document.getElementById("btn-write").innerHTML = "Done";
+
+    event.target.innerHTML = "done";
 };
