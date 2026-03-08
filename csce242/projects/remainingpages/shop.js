7efa7adb970330c1c892a5e05399aebd0eb30ce7
CSE-rezzieo <omoiguia@email.sc.edu>
2026-03-08 18:28:28 -0400

    update

---
+++ csce242/projects/remainingpages/shop.js
@@ -0,0 +1,32 @@
+fetch("products.json")
+  .then(response => response.json())
+  .then(data => {
+
+    const productGrid = document.querySelector(".product-grid");
+
+    productGrid.innerHTML = "";
+
+    data.products.forEach(product => {
+
+      const productCard = `
+        <a href="${product.page}" class="product-link">
+          <div class="product-card">
+            <div class="product-image">
+              <img src="${product.image}" alt="${product.name}">
+            </div>
+
+            <div class="product-info">
+              <h4>${product.name}</h4>
+              <p class="price">$${product.price}</p>
+              <span class="btn">View Item</span>
+            </div>
+          </div>
+        </a>
+      `;
+
+      productGrid.innerHTML += productCard;
+
+    });
+
+  })
+  .catch(error => console.error("Error loading products:", error));
